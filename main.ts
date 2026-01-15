13c95b9beb1a6ae122ec1550b72d4fde10d028cb
Andrew Foster <afoster1@gmail.com>
2026-01-15 18:33:22 +0000

    Address review bot comments.

--- main.ts
+++ main.ts
@@ -21,30 +21,30 @@ export default class SnorkelPlugin extends Plugin {
         await this.loadSettings();
 
         // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Snorkel Plugin', (evt: MouseEvent) => {
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Snorkel plugin', (evt: MouseEvent) => {
             // Called when the user clicks the icon.
-            new Notice('Hello from Snorkel!');
+            new Notice('Hello from snorkel!');
         });
         // Perform additional things with the ribbon
         ribbonIconEl.addClass('snorkel-ribbon-class');
 
         // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
         const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText('Status Bar Text');
+        statusBarItemEl.setText('Snorkel status');
 
         // This adds a simple command that can be triggered anywhere
         this.addCommand({
             id: 'open-sample-modal-simple',
             name: 'Open sample modal (simple)',
             callback: () => {
-                new SampleModal(this.app).open();
+                new SnorkelModal(this.app).open();
             }
         });
 
         // Daily Note command - creates test.md in configured folder
         this.addCommand({
             id: 'daily-note',
-            name: 'Daily Note',
+            name: 'Daily note',
             callback: async () => {
                 try {
                     // Get today's date in YYYY-MM-DD format
@@ -83,8 +83,19 @@ export default class SnorkelPlugin extends Plugin {
                                 return;
                             }
 
-                            const templater = (this.app as any).plugins.plugins['templater-obsidian'];
-                            if (templater) {
+                            const templaterPlugin = (this.app as unknown as { 
+                                plugins: { 
+                                    plugins: { 
+                                        'templater-obsidian'?: { 
+                                            templater: { 
+                                                overwrite_file_commands: (file: TFile) => Promise<void> 
+                                            } 
+                                        } 
+                                    } 
+                                } 
+                            }).plugins.plugins['templater-obsidian'];
+                            
+                            if (templaterPlugin) {
 
                                 // Wait for file to be fully loaded in the editor
                                 await new Promise(resolve => setTimeout(resolve, 100));
@@ -95,7 +106,7 @@ export default class SnorkelPlugin extends Plugin {
                                     const content = await this.app.vault.read(templateFile);
                                     await this.app.vault.modify(newFile, content);
                                     // Trigger Templater to process the file
-                                    await templater.templater.overwrite_file_commands(newFile);
+                                    await templaterPlugin.templater.overwrite_file_commands(newFile);
                                 } else {
                                     new Notice('Template file not found: ' + this.settings.dailyNoteTemplate);
                                 }
@@ -118,11 +129,10 @@ export default class SnorkelPlugin extends Plugin {
 
         // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
+            id: 'sample-editor',
+            name: 'Sample editor',
             editorCallback: (editor: Editor, view: MarkdownView) => {
-                console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
+                editor.replaceSelection('Sample Editor');
             }
         });
         // This adds a complex command that can check whether the current state of the app allows execution of the command
@@ -136,7 +146,7 @@ export default class SnorkelPlugin extends Plugin {
                     // If checking is true, we're simply "checking" if the command can be run.
                     // If checking is false, then we want to actually perform the operation.
                     if (!checking) {
-                        new SampleModal(this.app).open();
+                        new SnorkelModal(this.app).open();
                     }
 
                     // This command will only show up in Command Palette when the check function returns true
@@ -146,16 +156,7 @@ export default class SnorkelPlugin extends Plugin {
         });
 
         // This adds a settings tab so the user can configure various aspects of the plugin
-        this.addSettingTab(new SampleSettingTab(this.app, this));
-
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-            console.log('click', evt);
-        });
-
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        this.addSettingTab(new SnorkelSettingTab(this.app, this));
     }
 
     onunload() {
@@ -171,7 +172,7 @@ export default class SnorkelPlugin extends Plugin {
     }
 }
 
-class SampleModal extends Modal {
+class SnorkelModal extends Modal {
     constructor(app: App) {
         super(app);
     }
@@ -187,7 +188,7 @@ class SampleModal extends Modal {
     }
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class SnorkelSettingTab extends PluginSettingTab {
     plugin: SnorkelPlugin;
 
     constructor(app: App, plugin: SnorkelPlugin) {
@@ -201,21 +202,10 @@ class SampleSettingTab extends PluginSettingTab {
         containerEl.empty();
 
         new Setting(containerEl)
-            .setName('Setting #1')
-            .setDesc('It\'s a secret.')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
-
-        new Setting(containerEl)
-            .setName('Daily Note Folder')
+            .setName('Daily note folder')
             .setDesc('Folder where daily note will be created.')
             .addText(text => text
-                .setPlaceholder('folder/subfolder')
+                .setPlaceholder('Folder')
                 .setValue(this.plugin.settings.dailyNoteFolder)
                 .onChange(async (value) => {
                     this.plugin.settings.dailyNoteFolder = value;
@@ -223,10 +213,10 @@ class SampleSettingTab extends PluginSettingTab {
                 }));
 
         new Setting(containerEl)
-            .setName('Daily Note Template')
+            .setName('Daily note template')
             .setDesc('Path to template file to apply via Templater API (e.g., Templates/Daily Note.md). Leave empty to rely on Templater folder templates instead.')
             .addText(text => text
-                .setPlaceholder('Templates/Daily Note.md')
+                .setPlaceholder('Template path')
                 .setValue(this.plugin.settings.dailyNoteTemplate)
                 .onChange(async (value) => {
                     this.plugin.settings.dailyNoteTemplate = value;
